e570f5385e10d6929b533c30b535cbe8487eccd3
이광복 <galeb76@gmail.com>
2026-06-18 13:55:43 +0900

    Force overwrite to True when retrying failed files

--- app.py
+++ app.py
@@ -324,6 +324,7 @@ def convert():
     
     request_files = data.get('files', [])
     if request_files:
+        overwrite = True
         for f in request_files:
             file_path = input_path / f
             if file_path.exists() and file_path.is_file():
